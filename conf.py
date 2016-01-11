1b36952b7332cd24243ad4bbee8e03898131b51e
Brian King <brianjosephking@gmail.com>
2016-01-11 17:44:49 -0600

    test

--- conf.py
+++ conf.py
@@ -305,3 +305,11 @@ source_suffix = ['.rst', '.md']
 source_parsers = {
     '.md': CommonMarkParser,
 }
+
+github_doc_root = 'https://github.com/rtfd/recommonmark/tree/master/doc/'
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+            'url_resolver': lambda url: github_doc_root + url,
+            'auto_toc_tree_section': 'Contents',
+            }, True)
+    app.add_transform(AutoStructify)
