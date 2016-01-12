90ef5c06410e70c8ac8dd82c585487e999af829a
Brian King <brianjosephking@gmail.com>
2016-01-12 12:18:13 -0600

    test4

--- conf.py
+++ conf.py
@@ -305,11 +305,3 @@ source_suffix = ['.rst', '.md']
 source_parsers = {
     '.md': CommonMarkParser,
 }
-
-github_doc_root = 'https://github.com/brianjking/sphinx-test/tree/master/doc/'
-def setup(app):
-    app.add_config_value('recommonmark_config', {
-            'url_resolver': lambda url: github_doc_root + url,
-            'auto_toc_tree_section': 'Contents',
-            }, True)
-    app.add_transform(AutoStructify)
