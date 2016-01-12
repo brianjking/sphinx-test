4cfaef662ec7b5f43683b5b8ada6aa5e6ce952ef
Brian King <brianjosephking@gmail.com>
2016-01-12 12:14:56 -0600

    test 3

--- conf.py
+++ conf.py
@@ -306,7 +306,7 @@ source_parsers = {
     '.md': CommonMarkParser,
 }
 
-github_doc_root = 'https://github.com/rtfd/recommonmark/tree/master/doc/'
+github_doc_root = 'https://github.com/brianjking/sphinx-test/tree/master/doc/'
 def setup(app):
     app.add_config_value('recommonmark_config', {
             'url_resolver': lambda url: github_doc_root + url,
