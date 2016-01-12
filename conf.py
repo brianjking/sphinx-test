1c60c4655ac19d4b93ec353abcd12b0ca5262555
Brian King <brianjosephking@gmail.com>
2016-01-12 12:12:15 -0600

    Revert "Update conf.py"
    This reverts commit b0ce96a41a566b5354cb49e791707ecca621ce70.

--- conf.py
+++ conf.py
@@ -177,10 +177,10 @@ html_use_smartypants = True
 #html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-html_show_sourcelink = True
+#html_show_sourcelink = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-html_show_sphinx = False
+#html_show_sphinx = True
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
 #html_show_copyright = True
